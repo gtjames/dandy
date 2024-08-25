d8c503fe80803c8cfd9257897f89199232fcee07
gtjames <gtjames@gmail.com>
2024-08-24 19:16:40 -0500

    added comments

--- code.c
+++ code.c
@@ -7,16 +7,16 @@ int main(int argc, char *argv[]) {
     int first;
     int second;
     if (argc == 3) {
-        first  = atoi(argv[1]);
-        second = atoi(argv[2]);
+        first  = atoi(argv[1]);         //  two params are on the command line convert to int
+        second = atoi(argv[2]);         //  convert second param
     }
-    else if (argc == 2) {
-        first  = atoi(argv[1]);
-        second = getNumber("Enter 2nd #: ", 100);
+    else if (argc == 2) {               //  user has one param convert to int
+        first  = atoi(argv[1]);         //  conver the first (and only) command line param
+        second = getNumber("Enter 2nd #: ", 100);   //  prompt the user for the second number
     }
-    else if (argc == 1) {
-        first  = getNumber("Enter 1st #: ", 100);
-        second = getNumber("Enter 2nd #: ", 100);
+    else if (argc == 1) {               //  user did not enter any numbers on the command line
+        first  = getNumber("Enter 1st #: ", 100);   //  prompt the user for the first number
+        second = getNumber("Enter 2nd #: ", 100);   //  prompt the user for the second number
     }
 
     for (int i = first; i < second; i++) {
